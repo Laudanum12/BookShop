d82594c9c8fed56041a67e24f82c2cecc3cadda5
StayLog <39738891+StayLog@users.noreply.github.com>
2019-02-09 21:19:36 +0300

    n

--- BookShop/BookShop/routes/index.js
+++ BookShop/BookShop/routes/index.js
@@ -18,12 +18,96 @@ router.get('/task/new', async function (req, res) {
 router.get('/task/next', async function (req, res) {
 	res.render('page3', { title: 'Express', "data": getData() });
 });
-async function get_all_data() {
-	var sql_text = `select * from Каталог_Книга`;
+async function add_book(код_книги, количество_книг) {
+	var sql_text = `insert into Корзина_книга(код_книги, количество_книг) values(@код_книги, @количество_книг)`;
 
 	var connection = new sql.ConnectionPool({
 		database: 'Book_Shop',
-		server: 'DESKTOP-QJ276O0',
+		server: 'DESKTOP-QJ276O0\SQLEXPRESS',
+		driver: 'msnodesqlv8',
+		options: { trustedConnection: true }
+
+	});
+
+	await connection.connect();
+
+	var q_req = new sql.Request(connection);
+	var arr_tasks = await q_req
+		.input("код книги", sql.Int,код_книги)
+		.input("количество книг", sql.Int, priority)
+		.query(sql_text);
+
+}
+
+async function set_book_for_k(код_книги, количество_книг) {
+	var sql_text = `update Корзина_книга set количество_книг=@количество_книг where код_книги=@код_книги`;
+
+	var connection = new sql.ConnectionPool({
+		database: 'Book_Shop',
+		server: 'DESKTOP-QJ276O0\SQLEXPRESS',
+		driver: 'msnodesqlv8',
+		options: { trustedConnection: true }
+
+	});
+
+	await connection.connect();
+
+	var q_req = new sql.Request(connection);
+	var arr_tasks = await q_req
+		.input("код книги", sql.Int, код_книги)
+		.input("количество книг", sql.Int, количество_книг)
+		.query(sql_text);
+
+}
+
+
+async function delete_book(код_книги) {
+	var sql_text = `delete from Корзина_книга where код_книги = @код_книги`;
+
+	var connection = new sql.ConnectionPool({
+		database: 'Book_Shop',
+		server: 'DESKTOP-QJ276O0\SQLEXPRESS',
+		driver: 'msnodesqlv8',
+		options: { trustedConnection: true }
+
+	});
+
+	await connection.connect();
+
+	var q_req = new sql.Request(connection);
+	var arr_tasks = await q_req
+		.input("код книги", sql.Int, код_книги)
+		.query(sql_text);
+
+}
+
+async function find_book(код_книги) {
+	var sql_text = `select название, цена from Книга
+    where код_книги= @код_книги`;
+
+	var connection = new sql.ConnectionPool({
+		database: 'Book_Shop',
+		server: 'DESKTOP-QJ276O0\SQLEXPRESS',
+		driver: 'msnodesqlv8',
+		options: { trustedConnection: true }
+
+	});
+
+	await connection.connect();
+
+	var q_req = new sql.Request(connection);
+	var arr_tasks = await q_req.input('код_книги', sql.Int, код_книги).query(sql_text);
+
+	return arr_tasks.recordset;
+}
+// получить все задачи из базы данных
+async function get_all_data() {
+	var sql_text = `select название, цена, количество_просмотров_книги from Книга
+    order by название`;
+
+	var connection = new sql.ConnectionPool({
+		database: 'Book_Shop',
+		server: 'DESKTOP-QJ276O0\SQLEXPRESS',
 		driver: 'msnodesqlv8',
 		options: { trustedConnection: true }
 
@@ -36,69 +120,4 @@ async function get_all_data() {
 
 	return arr_tasks.recordset;
 }
-async function add_book(название, цена) {
-	var sql_text = `insert into Корзина_Книга(Название, Цена) values(@Название, @Цена)`;
-
-	var connection = new sql.ConnectionPool({
-		database: 'Book_Shop',
-
-		server: 'DESKTOP-QJ276O0',
-		driver: 'msnodesqlv8',
-		options: { trustedConnection: true }
-
-	});
-
-	await connection.connect();
-
-	var q_req = new sql.Request(connection);
-	var arr_tasks = await q_req
-		.input("Название", sql.varchar, Название)
-		.input("Цена", sql.money, Цена)
-		.query(sql_text);
-
-}
-async function delete_book() {
-	var sql_text = `delete Книга from Корзина_Книга where Книга = @код_книги`;
-
-	var connection = new sql.ConnectionPool({
-		database: 'Book_Shop',
-		server: 'DESKTOP-QJ276O0',
-		driver: 'msnodesqlv8',
-		options: { trustedConnection: true }
-
-	});
-
-	await connection.connect();
-
-	var q_req = new sql.Request(connection);
-	var arr_tasks = await q_req
-		.input("код_книги", sql.Int)
-		.query(sql_text);
-
-}
-//const mssql = require('mssql');
-//// Set database connection credentials
-//const config = {
-//	host: 'localhost',
-//	database: 'Book_Shop',
-//	server: 'DESKTOP-QJ276O0\SQLEXPRESS',
-//	driver: 'msnodesqlv8',
-//	options: { trustedConnection: true }
-//};
-////// Create a SQL pool
-//const pool = mssql.createPool(config);
-////// Export the pool
-//module.exports = pool;
-////// Load the SQL pool connection
-//	const pool = require('../data/config');
-////// Display all data
-//app.get('/Page2', (request, response) => {
-//	pool.query('SELECT * FROM Каталог_Книга', (error, result) => {
-//		if (error) throw error;
-
-//		response.send(result);
-//	});
-//});
-
-
 module.exports = router;
