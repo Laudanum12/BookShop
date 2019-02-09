7a591d04d9150ddc27ea8bb7d4ec6653643bba65
StayLog <39738891+StayLog@users.noreply.github.com>
2019-02-09 06:27:24 +0300

    Нужно догрузить недостающие модули. Какие вы грузили на 1 семе по node.js

--- BookShop/BookShop/routes/index.js
+++ BookShop/BookShop/routes/index.js
@@ -18,12 +18,31 @@ router.get('/task/new', async function (req, res) {
 router.get('/task/next', async function (req, res) {
 	res.render('page3', { title: 'Express', "data": getData() });
 });
-async function add_task(name, priority) {
-	var sql_text = `insert into Tasks(TaskName, Priority) values(@, @)`;
+async function get_all_data() {
+	var sql_text = `select * from Каталог_Книга`;
 
 	var connection = new sql.ConnectionPool({
-		database: '',
-		server: ' ',
+		database: 'Book_Shop',
+		server: 'DESKTOP-QJ276O0',
+		driver: 'msnodesqlv8',
+		options: { trustedConnection: true }
+
+	});
+
+	await connection.connect();
+
+	var q_req = new sql.Request(connection);
+	var arr_tasks = await q_req.query(sql_text);
+
+	return arr_tasks.recordset;
+}
+async function add_book(название, цена) {
+	var sql_text = `insert into Корзина_Книга(Название, Цена) values(@Название, @Цена)`;
+
+	var connection = new sql.ConnectionPool({
+		database: 'Book_Shop',
+
+		server: 'DESKTOP-QJ276O0',
 		driver: 'msnodesqlv8',
 		options: { trustedConnection: true }
 
@@ -33,9 +52,53 @@ async function add_task(name, priority) {
 
 	var q_req = new sql.Request(connection);
 	var arr_tasks = await q_req
-		.input("name", sql.NVarChar(120), name)
-		.input("priority", sql.Int, priority)
+		.input("Название", sql.varchar, Название)
+		.input("Цена", sql.money, Цена)
 		.query(sql_text);
 
 }
+async function delete_book() {
+	var sql_text = `delete Книга from Корзина_Книга where Книга = @код_книги`;
+
+	var connection = new sql.ConnectionPool({
+		database: 'Book_Shop',
+		server: 'DESKTOP-QJ276O0',
+		driver: 'msnodesqlv8',
+		options: { trustedConnection: true }
+
+	});
+
+	await connection.connect();
+
+	var q_req = new sql.Request(connection);
+	var arr_tasks = await q_req
+		.input("код_книги", sql.Int)
+		.query(sql_text);
+
+}
+//const mssql = require('mssql');
+//// Set database connection credentials
+//const config = {
+//	host: 'localhost',
+//	database: 'Book_Shop',
+//	server: 'DESKTOP-QJ276O0\SQLEXPRESS',
+//	driver: 'msnodesqlv8',
+//	options: { trustedConnection: true }
+//};
+////// Create a SQL pool
+//const pool = mssql.createPool(config);
+////// Export the pool
+//module.exports = pool;
+////// Load the SQL pool connection
+//	const pool = require('../data/config');
+////// Display all data
+//app.get('/Page2', (request, response) => {
+//	pool.query('SELECT * FROM Каталог_Книга', (error, result) => {
+//		if (error) throw error;
+
+//		response.send(result);
+//	});
+//});
+
+
 module.exports = router;
